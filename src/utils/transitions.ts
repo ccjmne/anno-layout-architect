0c0c98289c92c17550eda00905ca1e5d6706fb9c
ccjmne <ccjmne@gmail.com>
2020-06-28 22:39:50 +0200

    Create uniformised d3 transitions helpers

---
+++ src/utils/transitions.ts
@@ -0,0 +1,37 @@
+import { easeExpOut } from 'd3-ease';
+import { Selection } from 'd3-selection';
+import 'd3-transition';
+
+export function endOfMonth(date: Date): Date {
+  return new Date(date.getFullYear(), date.getMonth() + 1, 0);
+}
+
+export function snapTransition<E extends SVGElement, Datum>(
+  e: Selection<E, Datum, any, any>,
+  easing: (normalizedTime: number) => number = easeExpOut,
+) {
+  return e.transition().duration(75).ease(easing);
+}
+
+export function snapNamedTransition<E extends SVGElement, Datum>(
+  name: string,
+  e: Selection<E, Datum, any, any>,
+  easing: (normalizedTime: number) => number = easeExpOut,
+) {
+  return e.transition(name).duration(75).ease(easing);
+}
+
+export function slowTransition<E extends SVGElement, Datum>(
+  e: Selection<E, Datum, any, any>,
+  easing: (normalizedTime: number) => number = easeExpOut,
+) {
+  return e.transition().duration(500).ease(easing);
+}
+
+export function slowNamedTransition<E extends SVGElement, Datum>(
+  name: string,
+  e: Selection<E, Datum, any, any>,
+  easing: (normalizedTime: number) => number = easeExpOut,
+) {
+  return e.transition(name).duration(500).ease(easing);
+}
